bd5bd1ea302e82f1a929382afe586072561776ad
Lu9 <lucaslemmerick@hotmail.com>
2025-11-23 18:41:46 -0300

    Update comments.js

--- blog-assets/comments.js
+++ blog-assets/comments.js
@@ -65,7 +65,7 @@ if (document.body.classList.contains('post')) {
            <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24uTUe7W1XbtCoB9TFe6qNI1"+"&opts=16798&num=10&ts=1763893041482");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
           <!-- end www.htmlcommentbox.com -->
         `;
-        const targetElement = document.querySelector('main'));
+        const targetElement = document.querySelector('main');
 
         if (targetElement) {
             targetElement.insertAdjacentHTML('beforeend', widgetHTML);
